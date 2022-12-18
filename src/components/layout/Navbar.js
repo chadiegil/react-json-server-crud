c0d3394afea69cc3c017d6dfe0de3683081a97c3
unknown <chadiegilaugis@gmail.com>
2022-12-18 19:06:46 +0800

    init

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <Link className="navbar-brand" href="/">
+          Domain Course Final Project
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <Link className="btn btn-outline-light" to="/users/add">
+          Add User
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
